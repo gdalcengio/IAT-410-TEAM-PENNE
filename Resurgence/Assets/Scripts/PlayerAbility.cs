a13bc32139e61cd9560765addf8cc3fd26312540
Gabriele <gdalceng@sfu.ca>
2019-09-16 00:59:07 -0700

    [Gabe] Improved the feeling of movement

--- Resurgence/Assets/Scripts/PlayerAbility.cs
+++ Resurgence/Assets/Scripts/PlayerAbility.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class PlayerAbility : MonoBehaviour
 {
+    public bool canTranspose = false, canFissure = false;
     public Vector2 pushForce = new Vector2(0, 0);
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        //Debug.Log("enter");
+    }
     void OnTriggerStay2D(Collider2D col) {
+        //if (canTranspose) transpose();
+        //Debug.Log("staying");
+    }
+
+    public void transpose(Collider2D col) {
         if (Input.GetKeyDown("space")) {
             if (col.gameObject.tag == "Object") {
                 col.attachedRigidbody.AddForce(pushForce, ForceMode2D.Impulse);
--- Resurgence/Assets/Scripts/PlayerController.cs
+++ Resurgence/Assets/Scripts/PlayerController.cs
@@ -4,25 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 1f;
+    private Rigidbody2D rb;
+    public float maxSpeed;
+    public float moveForce = 0f;
+    public float jumpForce;
+    private int direction = 1;
+    //private float jump;
     private float translation = 0f;
 
     void Start()
     {
-        //rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        
 
-        translation = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        transform.Translate(translation, 0f, 0f, Space.World);
+        translation = Input.GetAxisRaw("Horizontal") * moveForce; //setting up movement
+        Debug.Log(translation);
+        if (Input.GetKey("d")) {
+            direction = 1;
+            transform.localScale = new Vector3(2.456f, transform.localScale.y, transform.localScale.z);
+            
+        }   
 
-        // horizontalForce.x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        // // Debug.Log(horizontalForce);
+        if (Input.GetKey("a")) {
+            direction = -1;
+            transform.localScale = new Vector3(-2.456f, transform.localScale.y, transform.localScale.z);
+        }
 
-        // if (rb.velocity.x < 10f && rb.velocity.x > -10f)
-        //     rb.AddForce(horizontalForce, ForceMode2D.Impulse);
-        // //Debug.Log(rb.velocity);
+        // Debug.LogError("direction: " + direction);
+        // Debug.LogError("velocity: " + rb.velocity.sqrMagnitude);
+        // Debug.LogError("translation: " + translation);
+        
+            if (transform.localScale.x < 0 && rb.velocity.x > 0 || transform.localScale.x > 0 && rb.velocity.x < 0) {          //if player is going right but wants left
+                
+                rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+            }
+
+        // if (Input.GetKeyDown("w")) {
+        //     StartCoroutine(example());
+        // }
+        movePlayer(); //actually moves the player
+    }
+
+    // private IEnumerator reference() {
+        
+    //     while (true) {
+    //         Debug.Log("test");
+    //         yield return new WaitForSeconds(1);
+    //     }
+    //     //yield return 0;
+    // }
+
+
+    private void movePlayer() {
+        //Limiting velocity
+        if (rb.velocity.sqrMagnitude > -maxSpeed && rb.velocity.sqrMagnitude < maxSpeed) rb.AddForce(Vector2.right * translation, ForceMode2D.Impulse);
+        if (Input.GetKeyDown("w")) rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); //jumping
     }
 }
