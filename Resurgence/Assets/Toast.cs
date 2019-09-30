8afa937d0116e9c3a82575bf0cee173cdf801a87
unknown <gdalceng@sfu.ca>
2019-09-30 13:50:56 -0700

    [Gabe] Added UIManager and tutorial/toast messages for prototype

---
+++ Resurgence/Assets/Toast.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Toast : MonoBehaviour
+{
+    private void OnTriggerEnter2D (Collider2D col){
+        if (col.gameObject.tag == "Player") {
+            Debug.LogError("starting coroutine");
+            IEnumerator toast = UIManager.Instance.Toast(3, GetComponent<Text>());
+            StartCoroutine(toast);
+            Debug.LogError("started coroutine");
+        }
+    }
+}
