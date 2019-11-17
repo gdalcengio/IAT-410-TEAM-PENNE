7c71bc86da18db821592324d363738fe9d5be1a9
Gabriele <gdalceng@sfu.ca>
2019-11-16 17:38:05 -0800

    [Gabe] Implemented anyButtonDown functionality to the cutscenes and start menu

---
+++ Resurgence/Assets/AnyButtonNext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnyButtonNext : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown) {
+            UIManager.Instance.fadeNextScene();
+        }
+    }
+}
