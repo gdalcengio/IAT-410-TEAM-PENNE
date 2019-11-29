a09ceb56dd4dee0bd99ef600fd165c3159fc52ef
Ryan <rdonnelly98@gmail.com>
2019-11-28 19:13:53 -0800

    *UNTESTED*
    - rocket flying animation
    - rocket explodes on contact and then is deleted

--- Resurgence/Assets/Scripts/Rocket.cs
+++ Resurgence/Assets/Scripts/Rocket.cs
@@ -8,6 +8,9 @@ public class Rocket : MonoBehaviour
     public float speed = 5f;
     public float rotateSpeed = 100f;
     private Rigidbody2D rb;
+    private bool active = true;
+
+    public Animator animator;
 
     void Start()
     {
@@ -22,21 +25,25 @@ public class Rocket : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 direction = (Vector2)target.position - rb.position;
-        direction.Normalize();
-        float rotateMag = Vector3.Cross(direction, transform.up).z;
+        if (active) {
+            Vector2 direction = (Vector2)target.position - rb.position;
+            direction.Normalize();
+            float rotateMag = Vector3.Cross(direction, transform.up).z;
 
-        rb.angularVelocity = -rotateMag * rotateSpeed;
-        rb.velocity = transform.up * speed;
+            rb.angularVelocity = -rotateMag * rotateSpeed;
+            rb.velocity = transform.up * speed;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        active = false;
+
         Debug.Log("test");
         if (col.gameObject.tag == "Itztli" || col.gameObject.tag == "Tlaloc") {
             GameManager.Instance.ResetScene();
         }
 
-        Destroy(transform.parent.gameObject);
+        animator.SetBool("Explode", true);
     }
 }
